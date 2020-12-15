0e53c39db7b6d34a887d08b96100684c1b5ad0a1
gundars-o <gundars.ozolins75@gmail.com>
2020-12-15 09:13:46 +0200

    Finished Author Routes

--- models/author.js
+++ models/author.js
@@ -1,10 +1,24 @@
 const mongoose = require( "mongoose" );
+const Book = require( "./book" );
 
 const authorSchema = new mongoose.Schema( {
     name: {
         type: String,
         required: true
     }
+} );
+
+/*Mongoose allows to do something before or after certain action occures.*/
+authorSchema.pre( "remove", function( next ) {
+    Book.find( { author: this.id }, ( err, books ) => {
+        if ( err ) {
+            next( err );
+        } else if ( books.length > 0 ) {
+            next( new Error( "This author has books still" ) );
+        } else {
+            next()/*ok to continue to remove the author*/;
+        };
+    } );
 } );
 
 module.exports = mongoose.model( "Author"/*name of table in database*/, authorSchema );
